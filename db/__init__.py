bb8c2578da73fe07fcb398d403af6ddbc73eed8f
jelchou <jchou@umass.edu>
2023-03-13 16:38:02 -0400

    models to access filtered dbs

--- db/__init__.py
+++ db/__init__.py
@@ -14,6 +14,42 @@ engine = create_engine(
     connect_args={"check_same_thread":False},
 )
 
+database2 = os.getenv("DATABASE_NAME", default="subreddit_submission_metadata")
+folder_path2 = os.getenv("DATABASE_FOLDER", default="./")
+version2 = os.getenv("DATABASE_VERSION", default="0.1.0")
+sqlite_path2 = f"{os.path.join(folder_path2,f'{database2}_{version2}')}.sqlite3"
+engine2 = create_engine(
+    f"sqlite:///{sqlite_path2}",
+    connect_args={"check_same_thread":False},
+)
+
+database3 = os.getenv("DATABASE_NAME", default="submission_content")
+folder_path3 = os.getenv("DATABASE_FOLDER", default="./")
+version3 = os.getenv("DATABASE_VERSION", default="0.1.0")
+sqlite_path3 = f"{os.path.join(folder_path3,f'{database3}_{version3}')}.sqlite3"
+engine3 = create_engine(
+    f"sqlite:///{sqlite_path3}",
+    connect_args={"check_same_thread":False},
+)
+
+database4 = os.getenv("DATABASE_NAME", default="filtered_200")
+folder_path4 = os.getenv("DATABASE_FOLDER", default="./")
+version4 = os.getenv("DATABASE_VERSION", default="0.1.0")
+sqlite_path4 = f"{os.path.join(folder_path4,f'{database4}_{version4}')}.sqlite3"
+engine4 = create_engine(
+    f"sqlite:///{sqlite_path4}",
+    connect_args={"check_same_thread":False},
+)
+
+database5 = os.getenv("DATABASE_NAME", default="filtered_300")
+folder_path5 = os.getenv("DATABASE_FOLDER", default="./")
+version5 = os.getenv("DATABASE_VERSION", default="0.1.0")
+sqlite_path5 = f"{os.path.join(folder_path5,f'{database5}_{version5}')}.sqlite3"
+engine5 = create_engine(
+    f"sqlite:///{sqlite_path5}",
+    connect_args={"check_same_thread":False},
+)
+
 def create_db():
     """Initialize db with an engine"""
     if folder_path != "" and not os.path.exists(folder_path):
