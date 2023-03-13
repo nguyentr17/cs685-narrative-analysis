702a72a4ca1c59c3b828b32364c836de3bd9e360
jelchou <jchou@umass.edu>
2023-03-13 16:37:19 -0400

    filtered for >= 200, 300 words

--- filter_submissions.py
+++ filter_submissions.py
@@ -1,4 +1,4 @@
-from db import engine
+from db import engine, engine2, engine3, engine4, engine5
 import pandas as pd
 from sqlalchemy import text
 
@@ -10,9 +10,13 @@ def filter_out(num_words):
     return df
 
 if __name__ == "__main__":
-    # filtered_300 = filter_out(300)
-    # filtered_300.to_sql('subreddit_submission_metadata', con=engine, if_exists='append', index=False)
-    filtered_200 = filter_out(200)
-    # filtered_200.to_sql('filtered_200', con=engine, if_exists='append', index=False)
-    # filtered_100 = filter_out(100)
-    # filtered_100.to_sql('subreddit_submission_metadata', con=engine, if_exists='append', index=False)
+    filtered_300 = filter_out(300)
+    filtered_300.to_sql('filtered_300', con=engine5, if_exists='append', index=False)
+    # filtered_200 = filter_out(200)
+    # filtered_200.to_sql('filtered_200', con=engine4, if_exists='append', index=False)
+    # metadata_query = "select * from subreddit_submission_metadata;"
+    # metadata_df = pd.read_sql_query(text(metadata_query), con=engine.connect())
+    # metadata_df.to_sql('subreddit_submission_metadata', con=engine2, if_exists='append', index=False)
+    # content_query = "select * from submission_content;"
+    # content_df = pd.read_sql_query(text(content_query), con=engine.connect())
+    # content_df.to_sql('submission_content', con=engine3, if_exists='append', index=False)
