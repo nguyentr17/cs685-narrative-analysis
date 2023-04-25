fe5a31082c0d9fc7de9f458a6b4f97aabc1d9017
AnthonyCampbell208 <78286293+AnthonyCampbell208@users.noreply.github.com>
2023-04-25 18:14:04 -0400

    Better changes, faster output, better results???
    Signed-off-by: AnthonyCampbell208 <78286293+AnthonyCampbell208@users.noreply.github.com>

--- clinical-extractions/chatgpt.py
+++ clinical-extractions/chatgpt.py
@@ -3,15 +3,31 @@ import os
 from utils import *
 import pdb
 from tqdm import tqdm
+import os
 
 tqdm.pandas()
-
 openai.api_key = os.getenv('OPENAI_API_KEY')
 narrative_df = pd.read_csv('narrative_detection/narrative_posts_by_trained_classification.csv')
-test_df = narrative_df.tail(5)
-test_df = test_df.progress_apply(apply_chatgpt, axis=1, prompt=get_new_instruction_prompt(), include_examples=True)
-# pdb.set_trace()
-unique_filename = get_unique_filename()
-test_df.to_csv(unique_filename)
+narrative_df = narrative_df.head(200)
+
+results_file = "clinical-extractions/results/prompt_fewshot.csv"
+if os.path.exists(results_file):
+    completed_df = pd.read_csv(results_file)
+    start_row = len(completed_df)
+    print(f'Starting at row {start_row}')
+else:
+    completed_df = pd.DataFrame()
+    start_row = 0
+
+chunk_size = 5
+
+total_rows = len(narrative_df)
+
+for i in range(start_row, total_rows, chunk_size):
+    chunk_df = narrative_df.iloc[i:i+chunk_size]
+    chunk_df = chunk_df.progress_apply(apply_chatgpt, axis=1, prompt=get_new_instruction_prompt(), include_examples=True)
+    completed_df = pd.concat([completed_df, chunk_df], ignore_index=True)
+    completed_df.to_csv(results_file, index=False)
+    print(f"Completed rows: {len(completed_df)}")
 
 
--- clinical-extractions/utils.py
+++ clinical-extractions/utils.py
@@ -1,6 +1,7 @@
 import openai
 import os
 import pdb
+import time
 
 def get_old_instruction_prompt():
   return """I am going to be giving you narratives about people with eating disorders. 
@@ -73,11 +74,11 @@ def get_few_shot_examples():
 
     Narrative: Idk who to talk to about this so I resort to reddit.I have been struggling a lot. I went to a party this weekend I threw up as soon as I had drank or ate anything. I threw up three times that night. It wasn't from the alcohol. I think my habits have just been destroying my stomach. I go through periods where I feel like can't stop eating and then a flip will switch and I can't even think about eating without feeling disgusting and nauseous. My stomach has hurt a lot more frequently and I get constant dizzy spells. I can't even think about getting dinner without crying. I also think that one of my friends are basically bragging about their eating disorder and it's triggering me to be worse.
 
-    {'factors': 'Friends bragging about eating disorders.', 'effect_details': 'The writer is experiencing physical and emotional symptoms related to their eating disorder, including stomach pain, nausea, and dizziness. 'Friends bragging is triggering their own eating disorder, 'effect_type': 'harmful' }
+    {'factors': 'Friends bragging about eating disorders.', 'effect_details': 'effect_details':['The writer is experiencing physical and emotional symptoms related to their eating disorder, including stomach pain, nausea, and dizziness.', 'Friends bragging is triggering their own eating disorder of binge eating'], 'effect_type': 'harmful' }
 
     Narrative: alright, so, well i dont know how to phrase this well, but my girlfriend has been going through a tough time recently with a vv big relapse on her ED recovery journey, and its quite apparent that she finds it hard to recover again. she seems quite drained and tired from all of that, and she does not want to talk to a counsellor about it as she is afraid her parents would be informed. ive been trying my best to emotionally support her, just watch out for her and stuff like that! i encourage her to eat, but i dont force! same for recovery and all that... its quite emotionally draining for me too, but i can cope with that alright! so that is alright on my end... i just would like to know from all of you here, what motivated you to recover? how can a loved one best support someone suffering from an ED? what is something you wish someone would say or do when you are in recovery?
 
-    {'factors': 'Emotional support from their partner, encouragement to eat but not forcing it', 'effect_details': 'Boyfriend providing support to girlfriend during rough time. Not forcing girlfriend to eat. They are also seeking advice on how to best support their loved one and what can help someone recover from an eating disorder', 'effect_type': 'helpful'}
+    {'factors': 'Emotional support from their partner', 'effect_details': 'Boyfriend is posting about girlfriend's eating disorder and saying that he is supporting her, but it is not obvious what the effect of his support has on on his girlfriend', 'effect_type': 'neutral'}
 
     Narrative: I started Concerta for my ADHD, and taking it daily has changed my life. I can do the normal life things I need to do, but notably it is a HUGE help with my binging disorder. The medication really helps me to manage impulses surrounding binging. \n\nIt also completely erases my appetite, but I'm making the effort to have multiple healthy meals every day so I'm not accidentally starving myself. So far it's been one solid month and I've lost around 10lbs! That sounds like a lot, but my starting weight was 315lbs, so this needs to happen for my health and wellness. Looking forward to getting into the 200s again
 
@@ -92,50 +93,101 @@ def get_few_shot_examples():
     {'factors': 'negative comments on baby's weight', 'effect_details': ['upsetting and triggering for the girlfriend who is recovering from an eating disorder'], 'effect_type': 'harmful'}
     """
 
-def apply_chatgpt(row, prompt, include_examples):
+# def apply_chatgpt(row, prompt, include_examples):
+#     narrative = row['selftext']
+#     max_retries = 2
+#     first_time_fail = False
+#     for i in range(1, 4):
+#         retries = 0
+#         while retries < max_retries:
+#             try:
+#                 if first_time_fail: 
+#                     row[f'factors_{i}'] = None
+#                     row[f'effect_type_{i}'] = None
+#                     row[f'effect_details_{i}'] = None
+#                     return row
+#                 if include_examples:
+#                     response = openai.ChatCompletion.create(
+#                         model="gpt-3.5-turbo",
+#                         n = 3,
+#                         messages=[
+#                                 {"role": "user", "content":prompt},
+#                                 {"role": "user", "content":get_few_shot_examples()},
+#                                 {"role": "user", "content": "Narrative: " + narrative}])
+#                 else:
+#                     response = openai.ChatCompletion.create(
+#                     model="gpt-3.5-turbo",
+#                     n = 3,
+#                     messages=[
+#                             {"role": "user", "content":prompt},
+#                             {"role": "user", "content": narrative},])
+#                 result = response.choices[0].message.content
+#                 # print(result)
+#                 result = eval(result)
+#                 row[f'factors_{i}'] = result['factors']
+#                 row[f'effect_type_{i}'] = result['effect_type']
+#                 row[f'effect_details_{i}'] = result['effect_details']
+#                 break
+#             except Exception as e:
+#                 print(e)
+#                 retries += 1
+#                 # print('failed')
+#                 # print(result)
+#                 if retries == max_retries:
+#                     row[f'factors_{i}'] = None
+#                     row[f'effect_type_{i}'] = None
+#                     row[f'effect_details_{i}'] = None
+#                     first_time_fail = True
+#     return row
+
+def has_correct_keys(dictionary):
+    keys_to_check = ['factors', 'effect_type', 'effect_details']
+    return all(key in dictionary for key in keys_to_check)
+
+def apply_chatgpt(row, prompt, include_examples, n = 5):
     narrative = row['selftext']
     max_retries = 2
     first_time_fail = False
-    for i in range(1, 4):
-        retries = 0
-        while retries < max_retries:
-            try:
-                if first_time_fail: 
-                    row[f'factors_{i}'] = None
-                    row[f'effect_type_{i}'] = None
-                    row[f'effect_details_{i}'] = None
-                    return row
-                if include_examples:
-                    response = openai.ChatCompletion.create(
-                        model="gpt-3.5-turbo",
-                        messages=[
-                                {"role": "user", "content":prompt},
-                                {"role": "user", "content":get_few_shot_examples()},
-                                {"role": "user", "content": "Narrative: " + narrative}])
-                else:
-                    response = openai.ChatCompletion.create(
-                    model="gpt-3.5-turbo",
-                    messages=[
-                            {"role": "user", "content":prompt},
-                            {"role": "user", "content": narrative},])
-                result = response.choices[0].message.content
-                # print(result)
-                result = eval(result)
-                row[f'factors_{i}'] = result['factors']
-                row[f'effect_type_{i}'] = result['effect_type']
-                row[f'effect_details_{i}'] = result['effect_details']
-                break
-            except Exception as e:
-                print(e)
-                retries += 1
-                # print('failed')
-                # print(result)
-                if retries == max_retries:
-                    row[f'factors_{i}'] = None
-                    row[f'effect_type_{i}'] = None
-                    row[f'effect_details_{i}'] = None
-                    first_time_fail = True
+    if include_examples:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            n = n,
+            messages=[
+                    {"role": "user", "content":prompt},
+                    {"role": "user", "content":get_few_shot_examples()},
+                    {"role": "user", "content": "Narrative: " + narrative}])
+    else:
+        response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        n = n,
+        messages=[
+                {"role": "user", "content":prompt},
+                {"role": "user", "content": narrative},])
+    count = 0
+    # pdb.set_trace()
+    for i in range(n):
+        result = response.choices[i].message.content
+        try:
+            result = eval(result)
+            if has_correct_keys(result):
+                row[f'factors_{count}'] = result['factors']
+                row[f'effect_type_{count}'] = result['effect_type']
+                row[f'effect_details_{count}'] = result['effect_details']
+            # print(result)
+            count += 1
+        except Exception as e:
+            print(e)
+        if count == 3:
+            time.sleep(20)
+            return row
+    while (count < 3):
+        row[f'factors_{count}'] = None
+        row[f'effect_type_{count}'] = None
+        row[f'effect_details_{count}'] = None
+        count += 1
+    time.sleep(20)
     return row
+
 
 def get_unique_filename(base_path = "clinical-extractions/results", base_filename = "prompt_fewshot", extension = ".csv"):
     counter = 1
